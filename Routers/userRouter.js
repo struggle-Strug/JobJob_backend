8b0f25eb3c947337bbfa6499d310fbae2bdcc74f
struggle-Strug <strugstruggle0430@gmail.com>
2025-01-10 04:31:58 +0900

    設定ページ完成

--- Routers/userRouter.js
+++ Routers/userRouter.js
@@ -8,6 +8,7 @@ router.post("/login", UserCtr.login);
 router.post("/:id/update", requireAuth, UserCtr.update);
 router.post("/:id/update/work_history", requireAuth, UserCtr.updateWork);
 router.post("/:id/update/desire", requireAuth, UserCtr.updateDesire);
+router.post("/delete/:id", requireAuth, UserCtr.deleteAccount);
 router.get("/tokenlogin", requireAuth, UserCtr.tokenlogin);
 
 module.exports = router;
