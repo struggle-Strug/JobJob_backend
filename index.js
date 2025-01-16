6b8f08b131d70a6a3d8a80eeb201229e843ded71
struggle-Strug <strugstruggle0430@gmail.com>
2025-01-17 04:18:43 +0900

    add facility and Jobpost page

--- index.js
+++ index.js
@@ -24,12 +24,19 @@ app.use('/uploads', express.static('uploads'));
 
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => console.log("Connected to MongoDB"))
-.catch((err) => console.log(err));
+const connectToMongoDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error("Error connecting to MongoDB:", err.message);
+  }
+};
+
+connectToMongoDB();
 
 // Passport
 app.use(passport.initialize());
