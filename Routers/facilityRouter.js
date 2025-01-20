a3ea12ce012429db140aa97410cd7a9aaea84f78
struggle-Strug <strugstruggle0430@gmail.com>
2025-01-20 14:25:38 +0900

    fix facilitycontroller and model

--- Routers/facilityRouter.js
+++ Routers/facilityRouter.js
@@ -3,7 +3,9 @@ const facilityController = require("../Controllers/facilityController");
 const requireAuth = require("../Middleware/RequireAuth");
 
 router.post("/", requireAuth, facilityController.createFacility);
+router.post("/allow/:id", requireAuth, facilityController.allowFacility);
 router.get("/", requireAuth, facilityController.getFacilities);
+router.get("/all", requireAuth, facilityController.getAllFacilities);
 router.get("/:id", requireAuth, facilityController.getFacility);
 
 module.exports = router;
