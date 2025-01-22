a9b319c102dba8c865388a81c041f2506a84d603
struggle-Strug <strugstruggle0430@gmail.com>
2025-01-23 08:54:57 +0900

    add jobpost edit

--- Routers/facilityRouter.js
+++ Routers/facilityRouter.js
@@ -3,6 +3,7 @@ const facilityController = require("../Controllers/facilityController");
 const requireAuth = require("../Middleware/RequireAuth");
 
 router.post("/", requireAuth, facilityController.createFacility);
+router.post("/pending/:id", requireAuth, facilityController.pendingFacility);
 router.post("/allow/:id", requireAuth, facilityController.allowFacility);
 router.put("/:id", requireAuth, facilityController.updateFacility);
 router.get("/", requireAuth, facilityController.getFacilities);
