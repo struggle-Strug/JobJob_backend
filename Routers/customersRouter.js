ae67ff29c52f3d6cb058cd71844457b06f3bcf0c
struggle-Strug <strugstruggle0430@gmail.com>
2025-02-07 04:13:39 +0900

    ユーザー管理完成

--- Routers/customersRouter.js
+++ Routers/customersRouter.js
@@ -1,9 +1,15 @@
 const router = require("express").Router();
+const requireAuth = require("../Middleware/requireAuth");
 const customersController = require("../Controllers/customersController");
 
-router.post("/signup", customersController.signup);
-router.post("/signin", customersController.signin);
-router.get("/tokenlogin", customersController.tokenlogin);
-router.get("/all", customersController.getAllCustomers);
-router.get("/:id", customersController.getCustomerById);
+router.post("/signup", requireAuth, customersController.signup);
+router.post("/signin", requireAuth, customersController.signin);
+router.post("/users", requireAuth, customersController.addUser);
+router.put("/email/:id", requireAuth, customersController.updateEmail);
+router.put("/password/:id", requireAuth, customersController.updatePassword);
+router.delete("/users/:id", requireAuth, customersController.deleteUser);
+router.get("/tokenlogin", requireAuth, customersController.tokenlogin);
+router.get("/users", requireAuth, customersController.getUsers);
+router.get("/all", requireAuth, customersController.getAllCustomers);
+router.get("/:id", requireAuth, customersController.getCustomerById);
 module.exports = router;
