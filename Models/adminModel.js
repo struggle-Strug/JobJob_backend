b85fba57a2d696652c845f2dff29946438760bd5
struggle-Strug <strugstruggle0430@gmail.com>
2025-02-14 23:07:15 +0900

    add: Add Setting Page

--- Models/adminModel.js
+++ Models/adminModel.js
@@ -21,31 +21,23 @@ const AdminSchema = new mongoose.Schema({
   },
 });
 
-// Pre-hook to hash the password before saving
-AdminSchema.pre("save", async function (next) {
-  if (this.isModified("password")) {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-  }
-  next();
-});
-
 // Check if model exists before creating it to avoid model redefinition
 const Admin = mongoose.models.Admin || mongoose.model("Admin", AdminSchema);
 
 // Function to seed a default admin user
 const seedDefaultAdmin = async () => {
-  const defaultAdmin = {
-    loginId: "admin",
-    name: "システム管理者",
-    password: "admin123", // This will be hashed in the pre-hook
-  };
-
   try {
-    const adminExists = await Admin.findOne({ loginId: defaultAdmin.loginId });
+    const adminExists = await Admin.findOne({ loginId: "admin" });
+
     if (!adminExists) {
       console.log("Seeding default admin user...");
-      const admin = new Admin(defaultAdmin);
+      const hashedPassword = await bcrypt.hash("admin123", 10);
+      const admin = new Admin({
+        loginId: "admin",
+        name: "システム管理者",
+        password: hashedPassword,
+      });
+
       await admin.save();
       console.log("Default admin user created successfully!");
     } else {
