acbc21bf81d73da12aa98098681da4ce7e65edd2
struggle-Strug <strugstruggle0430@gmail.com>
2025-02-14 18:28:15 +0900

    add:CSManagement Page

--- Controllers/messageController/index.js
+++ Controllers/messageController/index.js
@@ -5,175 +5,354 @@ const MessageModel = require("../../Models/MessageModel");
 const UserModel = require("../../Models/UserModel");
 
 exports.save = async (req, res) => {
-    try{
-        const { jobPost_id, sender, recevier, content } = req.body;
-        const messages = await MessageModel.find({});
-        const message = await MessageModel.find({ jobPost_id: jobPost_id, sender: sender});
-        
-        if(message.length > 0) return res.json({ message: "応募済みです。", error: true });
-        const newMessage = new MessageModel({
-            message_id: messages.length + 1,
-            first: sender,
-            second: recevier,
-            jobPost_id: jobPost_id,
-            content: [
-                {
-                    sender: sender,
-                    recevier: recevier,
-                    message: content,
-                    date: Date.now()
-                }
-            ]
-        });
-        newMessage.save()
-            .then((message) => {
-            res.json({message: message});
-        })
-    }catch(error){
-        res.status(500).json({ message: "サーバーエラー", error: true });
-    }
-}
+  try {
+    const { jobPost_id, sender, recevier, content } = req.body;
+    const messages = await MessageModel.find({});
+    const message = await MessageModel.find({
+      jobPost_id: jobPost_id,
+      sender: sender,
+    });
+
+    if (message.length > 0)
+      return res.json({ message: "応募済みです。", error: true });
+    const newMessage = new MessageModel({
+      message_id: messages.length + 1,
+      first: sender,
+      second: recevier,
+      jobPost_id: jobPost_id,
+      content: [
+        {
+          sender: sender,
+          recevier: recevier,
+          message: content,
+          date: Date.now(),
+        },
+      ],
+    });
+    newMessage.save().then((message) => {
+      res.json({ message: message });
+    });
+  } catch (error) {
+    res.status(500).json({ message: "サーバーエラー", error: true });
+  }
+};
 
 exports.getMine = async (req, res) => {
-    try{
-        const id = req.params.id;
-        const messages = await MessageModel.find({
-            $or: [{ first: id }, { second: id }],
-        });
+  try {
+    const id = req.params.id;
+    const messages = await MessageModel.find({
+      $or: [{ first: id }, { second: id }],
+    });
 
-        const messageWithDetails = await Promise.all(
-            messages.map(async (message) => {
-                const jobPost = await JobPostModel.findOne({ jobpost_id: message.jobPost_id });
-                const facility = await FacilityModel.findOne({ facility_id: jobPost.facility_id });
-                const customer = await CustomerModel.findOne({ customer_id: jobPost.customer_id });
-                return {
-                    ...message.toObject(), // Convert MongoDB document to plain object
-                    customer_id: customer, // Include customer data
-                    facility_id: facility, // Include facility data
-                };
-            })
-        );
-          
-        res.json({messages: messageWithDetails});
-    }catch(error){
-        console.log(error);
-        
-        res.status(500).json({ message: "サーバーエラー", error: true });
-    }
-}
+    const messageWithDetails = await Promise.all(
+      messages.map(async (message) => {
+        const jobPost = await JobPostModel.findOne({
+          jobpost_id: message.jobPost_id,
+        });
+        const facility = await FacilityModel.findOne({
+          facility_id: jobPost.facility_id,
+        });
+        const customer = await CustomerModel.findOne({
+          customer_id: jobPost.customer_id,
+        });
+        return {
+          ...message.toObject(), // Convert MongoDB document to plain object
+          customer_id: customer, // Include customer data
+          facility_id: facility, // Include facility data
+        };
+      })
+    );
+
+    res.json({ messages: messageWithDetails });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({ message: "サーバーエラー", error: true });
+  }
+};
+
+exports.getAllMessages = async (req, res) => {
+  try {
+    const messages = await MessageModel.find({});
+
+    const messageWithDetails = await Promise.all(
+      messages.map(async (message) => {
+        const jobPost = await JobPostModel.findOne({
+          jobpost_id: message.jobPost_id,
+        });
+        const facility = await FacilityModel.findOne({
+          facility_id: jobPost.facility_id,
+        });
+        return {
+          ...message.toObject(), // Convert MongoDB document to plain object
+          facility_id: facility, // Include facility data
+        };
+      })
+    );
+
+    res.json({ messages: messageWithDetails });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({ message: "サーバーエラー", error: true });
+  }
+};
 
 exports.getById = async (req, res) => {
-    try{
-        const id = req.params.id;
-        const message = await MessageModel.findOne({ message_id: id });
-        
-        const jobPost = await JobPostModel.findOne({ jobpost_id: message.jobPost_id });
-        const facility = await FacilityModel.findOne({ facility_id: jobPost.facility_id });
-        const customer = await CustomerModel.findOne({ customer_id: jobPost.customer_id });
+  try {
+    const id = req.params.id;
+    const message = await MessageModel.findOne({ message_id: id });
 
-        res.json({message: {...message.toObject(), customer_id: customer, facility_id: facility, jobpost_id: jobPost}});
-    }catch(error){
-        console.log(error);
-        res.status(500).json({ message: "サーバーエラー", error: true });
-    }
-}
+    const jobPost = await JobPostModel.findOne({
+      jobpost_id: message.jobPost_id,
+    });
+    const facility = await FacilityModel.findOne({
+      facility_id: jobPost.facility_id,
+    });
+    const customer = await CustomerModel.findOne({
+      customer_id: jobPost.customer_id,
+    });
+
+    res.json({
+      message: {
+        ...message.toObject(),
+        customer_id: customer,
+        facility_id: facility,
+        jobpost_id: jobPost,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "サーバーエラー", error: true });
+  }
+};
 
 exports.send = async (req, res) => {
-    try{
-        const { message_id, sender, message, recevier, files } = req.body;
-        
-        const messages = await MessageModel.findOne({ message_id: message_id });
-        
-        const newMessage = {
-            sender: sender,
-            recevier: recevier,
-            files: files,
-            message: message,
-        }
-        await messages.content.push(newMessage);
-        await messages.save()
-            .then((message) => {
-            res.json({message: message});
-        })
-    }catch(error){
-        console.log(error);
-        res.status(500).json({ message: "サーバーエラー", error: true });
-    }
-}
+  try {
+    const { message_id, sender, message, recevier, files } = req.body;
+
+    const messages = await MessageModel.findOne({ message_id: message_id });
+
+    const newMessage = {
+      sender: sender,
+      recevier: recevier,
+      files: files,
+      message: message,
+    };
+    await messages.content.push(newMessage);
+    await messages.save().then((message) => {
+      res.json({ message: message });
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "サーバーエラー", error: true });
+  }
+};
 
 exports.getJobNumbersByStatus = async (req, res) => {
-    try{
-        const messages = await MessageModel.find({});
-        const jobNumbers = {
-            allOnGoings: messages.filter((message) => message.status === "応募済" || message.status === "書類選考中" || message.status === "面接日設定済" || message.status === "面接実施中" || message.status === "内定済" || message.status === "内定承諾済").length,
-            allEnds: messages.filter((message) => message.status === "入職済" || message.status === "不採用" || message.status === "内定辞退" || message.status === "選考終了").length,
-            応募済: messages.filter((message) => message.status === "応募済").length,
-            書類選考中: messages.filter((message) => message.status === "書類選考中").length,
-            面接日設定済: messages.filter((message) => message.status === "面接日設定済").length,
-            面接実施中: messages.filter((message) => message.status === "面接実施中").length,
-            内定済: messages.filter((message) => message.status === "内定済").length,
-            内定承諾済: messages.filter((message) => message.status === "内定承諾済").length,
-            入職済: messages.filter((message) => message.status === "入職済").length,
-            不採用: messages.filter((message) => message.status === "不採用").length,
-            内定辞退: messages.filter((message) => message.status === "内定辞退").length,
-            選考終了: messages.filter((message) => message.status === "選考終了").length,
-        }
-        return res.json({ message: "Successfully get JobNumbers", jobNumbers: jobNumbers})
-    }catch(error){
-        console.log(error);
-        res.status(500).json({ message: "サーバーエラー", error: true });
-    }
-}
+  try {
+    const messages = await MessageModel.find({
+      $or: [{ first: req.user.data._id }, { second: req.user.data._id }],
+    });
+    const jobNumbers = {
+      allOnGoings: messages.filter(
+        (message) =>
+          message.status === "応募済" ||
+          message.status === "書類選考中" ||
+          message.status === "面接日設定済" ||
+          message.status === "面接実施中" ||
+          message.status === "内定済" ||
+          message.status === "内定承諾済"
+      ).length,
+      allEnds: messages.filter(
+        (message) =>
+          message.status === "入職済" ||
+          message.status === "不採用" ||
+          message.status === "内定辞退" ||
+          message.status === "選考終了"
+      ).length,
+      応募済: messages.filter((message) => message.status === "応募済").length,
+      書類選考中: messages.filter((message) => message.status === "書類選考中")
+        .length,
+      面接日設定済: messages.filter(
+        (message) => message.status === "面接日設定済"
+      ).length,
+      面接実施中: messages.filter((message) => message.status === "面接実施中")
+        .length,
+      内定済: messages.filter((message) => message.status === "内定済").length,
+      内定承諾済: messages.filter((message) => message.status === "内定承諾済")
+        .length,
+      入職済: messages.filter((message) => message.status === "入職済").length,
+      不採用: messages.filter((message) => message.status === "不採用").length,
+      内定辞退: messages.filter((message) => message.status === "内定辞退")
+        .length,
+      選考終了: messages.filter((message) => message.status === "選考終了")
+        .length,
+    };
+    return res.json({
+      message: "Successfully get JobNumbers",
+      jobNumbers: jobNumbers,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "サーバーエラー", error: true });
+  }
+};
+
+exports.getAllJobNumbersByStatus = async (req, res) => {
+  try {
+    const messages = await MessageModel.find({});
+    const jobNumbers = {
+      allOnGoings: messages.filter(
+        (message) =>
+          message.status === "応募済" ||
+          message.status === "書類選考中" ||
+          message.status === "面接日設定済" ||
+          message.status === "面接実施中" ||
+          message.status === "内定済" ||
+          message.status === "内定承諾済"
+      ).length,
+      allEnds: messages.filter(
+        (message) =>
+          message.status === "入職済" ||
+          message.status === "不採用" ||
+          message.status === "内定辞退" ||
+          message.status === "選考終了"
+      ).length,
+      応募済: messages.filter((message) => message.status === "応募済").length,
+      書類選考中: messages.filter((message) => message.status === "書類選考中")
+        .length,
+      面接日設定済: messages.filter(
+        (message) => message.status === "面接日設定済"
+      ).length,
+      面接実施中: messages.filter((message) => message.status === "面接実施中")
+        .length,
+      内定済: messages.filter((message) => message.status === "内定済").length,
+      内定承諾済: messages.filter((message) => message.status === "内定承諾済")
+        .length,
+      入職済: messages.filter((message) => message.status === "入職済").length,
+      不採用: messages.filter((message) => message.status === "不採用").length,
+      内定辞退: messages.filter((message) => message.status === "内定辞退")
+        .length,
+      選考終了: messages.filter((message) => message.status === "選考終了")
+        .length,
+    };
+    return res.json({
+      message: "Successfully get JobNumbers",
+      jobNumbers: jobNumbers,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "サーバーエラー", error: true });
+  }
+};
 
 exports.getByStatus = async (req, res) => {
-    try{
-        const id = req.params.id;
-        const status = req.params.status;
+  try {
+    const id = req.params.id;
+    const status = req.params.status;
 
-        const messages = await MessageModel.find({
-            $or: [{ first: id }, { second: id }],
-        });
+    const messages = await MessageModel.find({
+      $or: [{ first: id }, { second: id }],
+    });
 
-        let filteredMessages;
-        if(status === "allOnGoings") {
-            filteredMessages = messages
-        } else if(status === "allEnds"){
-            filteredMessages = messages.filter((message) => message.status === ("入職済" || "不採用" || "内定辞退" || "選考終了"));
-        } else {
-            filteredMessages = messages.filter((message) => message.status === status);
-        }
-
-        const processes = await Promise.all(
-            filteredMessages.map(async (message) => {
-                const jobPost = await JobPostModel.findOne({ jobpost_id: message.jobPost_id });
-                const facility = await FacilityModel.findOne({ facility_id: jobPost.facility_id });
-                const user_id = message.first === id ? message.second : message.first;
-                const user = await UserModel.findOne({ _id: user_id });
-                return {
-                    ...message.toObject(), // Convert MongoDB document to plain object
-                    facility_id: facility, // Include facility data
-                    jobpost_id: jobPost,
-                    user_id: user // Include user data
-                };
-            })
-        );
-
-        res.json({processes: processes});
-    }catch(error){
-        console.log(error);
-        res.status(500).json({ message: "サーバーエラー", error: true });
+    let filteredMessages;
+    if (status === "allOnGoings") {
+      filteredMessages = messages;
+    } else if (status === "allEnds") {
+      filteredMessages = messages.filter(
+        (message) =>
+          message.status === ("入職済" || "不採用" || "内定辞退" || "選考終了")
+      );
+    } else {
+      filteredMessages = messages.filter(
+        (message) => message.status === status
+      );
     }
-}
+
+    const processes = await Promise.all(
+      filteredMessages.map(async (message) => {
+        const jobPost = await JobPostModel.findOne({
+          jobpost_id: message.jobPost_id,
+        });
+        const facility = await FacilityModel.findOne({
+          facility_id: jobPost.facility_id,
+        });
+        const user_id = message.first === id ? message.second : message.first;
+        const user = await UserModel.findOne({ _id: user_id });
+        return {
+          ...message.toObject(), // Convert MongoDB document to plain object
+          facility_id: facility, // Include facility data
+          jobpost_id: jobPost,
+          user_id: user, // Include user data
+        };
+      })
+    );
+
+    res.json({ processes: processes });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "サーバーエラー", error: true });
+  }
+};
+
+exports.getAllByStatus = async (req, res) => {
+  try {
+    const status = req.params.status;
+
+    const messages = await MessageModel.find({});
+
+    let filteredMessages;
+    if (status === "allOnGoings") {
+      filteredMessages = messages;
+    } else if (status === "allEnds") {
+      filteredMessages = messages.filter(
+        (message) =>
+          message.status === ("入職済" || "不採用" || "内定辞退" || "選考終了")
+      );
+    } else {
+      filteredMessages = messages.filter(
+        (message) => message.status === status
+      );
+    }
+
+    const processes = await Promise.all(
+      filteredMessages.map(async (message) => {
+        const jobPost = await JobPostModel.findOne({
+          jobpost_id: message.jobPost_id,
+        });
+        const facility = await FacilityModel.findOne({
+          facility_id: jobPost.facility_id,
+        });
+        const user_id = message.content[0].sender;
+        const user = await UserModel.findOne({ _id: user_id });
+        return {
+          ...message.toObject(), // Convert MongoDB document to plain object
+          facility_id: facility, // Include facility data
+          jobpost_id: jobPost,
+          user_id: user, // Include user data
+        };
+      })
+    );
+
+    res.json({ processes: processes });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "サーバーエラー", error: true });
+  }
+};
 
 exports.updateMessage = async (req, res) => {
-    try{
-        const message = await MessageModel.findOneAndUpdate({ message_id: req.params.id }, req.body);
-        await message.save()
-            .then((message) => {
-            res.json({message: message});
-        })
-    }catch(error){
-        console.log(error);
-        res.status(500).json({ message: "サーバーエラー", error: true });
-    }
-}
+  try {
+    const message = await MessageModel.findOneAndUpdate(
+      { message_id: req.params.id },
+      req.body
+    );
+    await message.save().then((message) => {
+      res.json({ message: message });
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "サーバーエラー", error: true });
+  }
+};
--- Models/UserModel.js
+++ Models/UserModel.js
@@ -1,160 +1,176 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    member_id: {
+  member_id: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  hiraganaName: {
+    type: String,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+  },
+  birthday: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  jobType: [
+    {
+      type: {
         type: String,
-        required: true,
-    },
-    name: {
+      },
+      period: {
         type: String,
-        required: true,
+      },
     },
-    hiraganaName: {
+  ],
+  employmentType: {
+    type: Array,
+  },
+  employmentDate: {
+    type: String,
+  },
+  desireYearSalary: {
+    type: String,
+  },
+  qualification: [
+    {
+      qualification: { type: String, required: true },
+      year: { type: Number },
+      month: { type: Number },
+    },
+  ],
+  feature: {
+    type: Array,
+  },
+  dependents: {
+    type: Number,
+  },
+  spouse: {
+    type: String,
+  },
+  selfPR: {
+    type: String,
+  },
+  prefecture: {
+    type: String,
+  },
+  desirePrefecture: [
+    {
+      type: String,
+    },
+  ],
+  photo: {
+    type: String,
+  },
+  lastEducation: {
+    type: String,
+  },
+  schoolName: {
+    type: String,
+  },
+  department: {
+    type: String,
+  },
+  major: {
+    type: String,
+  },
+  graduation: {
+    type: String,
+  },
+  graduationDate: {
+    type: String,
+  },
+  workHistories: [
+    {
+      companyName: {
         type: String,
-        required: true,
-    },
-    gender: {
+      },
+      contents: {
         type: String,
-        required: true,
-    },
-    birthday: {
+      },
+      startDate: {
         type: String,
-        required: true,
-    },
-    email: {
+      },
+      endDate: {
         type: String,
-        required: true,
-    },
-    phoneNumber: {
+      },
+      employmentType: {
         type: String,
-        required: true,
-    },
-    password: {
+      },
+      jobType: {
         type: String,
-        required: true,
-    },
-    jobType: [
-        {
-            type: {
-                type: String
-            },
-            period: {
-                type: String
-            },
-        }
-    ],  
-    employmentType: {
-        type: Array,
-    },
-    employmentDate: {
+      },
+      jobTypeDetail: {
         type: String,
-    },
-    desireYearSalary: {
+      },
+      workContent: {
         type: String,
-    },
-    qualification: [{
-        qualification: { type: String, required: true },
-        year: { type: Number },
-        month: { type: Number },
-    }],
-    feature: {
-        type: Array,
-    },
-    dependents: {
-        type: Number,
-    },
-    spouse: {
+      },
+      officialPosition: {
         type: String,
-    },
-    selfPR: {
+      },
+      payType: {
         type: String,
-    },
-    prefecture: {
+      },
+      amount: {
         type: String,
+      },
     },
-    desirePrefecture: [{
-        type: String,
-    }],
-    photo: {
-        type: String,
+  ],
+  setting: {
+    notificationEmail: {
+      type: Boolean,
+      default: true, // Default value set to true
     },
-    lastEducation: {
-        type: String,
+    message: {
+      type: Boolean,
+      default: true, // Default value set to true
     },
-    schoolName: {
-        type: String,
+    newJob: {
+      type: Boolean,
+      default: true, // Default value set to true
     },
-    department: {
-        type: String,
+    recommendJob: {
+      type: Boolean,
+      default: true, // Default value set to true
     },
-    major: {
-        type: String,
-    },
-    graduation: {
-        type: String,
-    },
-    graduationDate: {
-        type: String,
-    },
-    workHistories: [
-        {
-            companyName: {
-                type: String,
-            },
-            contents: {
-                type: String,
-            },
-            startDate: {
-                type: String,
-            },
-            endDate: {
-                type: String,
-            },
-            employmentType: {
-                type: String,
-            },
-            jobType: {
-                type: String,
-            },
-            jobTypeDetail: {
-                type: String,
-            },
-            workContent: {
-                type: String,
-            },
-            officialPosition: {
-                type: String,
-            },
-            payType: {
-                type: String,
-            },
-            amount: {
-                type: String,
-            }
-        }
-    ],
-    setting: {
-        notificationEmail: {
-            type: Boolean,
-            default: true, // Default value set to true
-        },
-        message: {
-            type: Boolean,
-            default: true, // Default value set to true
-        },
-        newJob: {
-            type: Boolean,
-            default: true, // Default value set to true
-        },
-        recommendJob: {
-            type: Boolean,
-            default: true, // Default value set to true
-        },
-    },
-    role: {
-        type: String,
-        default: "member",
-    }
+  },
+  role: {
+    type: String,
+    default: "member",
+  },
+  deleted: {
+    type: Boolean,
+    default: false,
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+  updated_at: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 module.exports = mongoose.model("User", userSchema);
