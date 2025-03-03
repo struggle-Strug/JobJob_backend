797680ac6c3395b9c2589c1c0f8bb876b7d2e426
struggle-Strug <strugstruggle0430@gmail.com>
2025-03-03 15:24:45 +0200

    add:add delete facility controller

--- Routers/facilityRouter.js
+++ Routers/facilityRouter.js
@@ -6,6 +6,7 @@ router.post("/", requireAuth, facilityController.createFacility);
 router.post("/pending/:id", requireAuth, facilityController.pendingFacility);
 router.post("/allow/:id", requireAuth, facilityController.allowFacility);
 router.put("/:id", requireAuth, facilityController.updateFacility);
+router.delete("/:id", requireAuth, facilityController.deleteFacility);
 router.get("/getByCompany", requireAuth, facilityController.getAllFacilities);
 router.get("/all", requireAuth, facilityController.getAllFacilities);
 router.get("/:id", requireAuth, facilityController.getFacility);
